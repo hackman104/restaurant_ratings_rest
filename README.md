de530071a31cc7f7d165347870df6b1be436a1a0
Drew Stimson <drewstimson@Drews-MacBook-Air.local>
2019-01-06 22:15:08 -0500

    Initial commit for project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Restaurant Allergy Ratings REST API
+
+This is a [django-rest-framework](https://www.django-rest-framework.org/)-based REST API for storing and retrieving allergy information for restaurants.
---
+++ ratings/models.py
@@ -0,0 +1,145 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Restaurant(models.Model):
+    """
+    A model to track restaurant info
+    """
+    USA = 'USA'
+    CANADA = 'CA'
+    MEXICO = 'MEX'
+    COUNTRY_CHOICES = (
+        (USA, 'USA'),
+        (CANADA, 'Canada'),
+        (MEXICO, 'Mexico'),
+    )
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField("date added", default=timezone.now)
+    # last_review = models.DateTimeField("last review")
+    street_address = models.CharField(max_length=100, default=None, blank=True, null=True)
+    street_address_2 = models.CharField(max_length=30, default=None, blank=True, null=True)
+    city = models.CharField(max_length=50, default=None, blank=True, null=True)
+    state = models.CharField(max_langeth=2, default=None, blank=True, null=True)
+    country = models.CharField(max_length=50, choices=COUNTRY_CHOICES, default=USA)
+    phone_number = models.CharField(max_length=15, default=None, blank=True, null=True)
+    website = models.CharField(max_length=100, default=None, blank=True, null=True)
+    google_maps_link = models.CharField(max_length=300, default=None, blank=True, null=True)
+
+    @property
+    def latest_review(self):
+        return self.review_set.latest('review_date')
+
+    class Meta:
+        ordering = ('name',)
+
+class Review(models.Model):
+    """
+    A model that tracks restaurant reviews
+    """
+    ZERO_STAR = 0
+    ONE_STAR = 1
+    TWO_STAR = 2
+    THREE_STAR = 3
+    FOUR_STAR = 4
+    FIVE_STAR = 5
+    STAR_CHOICES = (
+        (ZERO_STAR, '0'),
+        (ONE_STAR, '1'),
+        (TWO_STAR, '2'),
+        (THREE_STAR, '3'),
+        (FOUR_STAR, '4'),
+        (FIVE_STAR, '5'),
+    )
+    MILK = 'milk'
+    SOY = 'soy'
+    PEANUT = 'peanut'
+    WHEAT = 'wheat'
+    TREE_NUTS = 'tree nuts'
+    EGG = 'egg'
+    SHELLFISH = 'shellfish'
+    FISH = 'fish'
+    SESAME = 'sesame'
+    ALLERGEN_CHOICES = (
+        (MILK, 'Milk'),
+        (SOY, 'Soy'),
+        (PEANUT, 'Peanuts'),
+        (WHEAT, 'Wheat/Gluten'),
+        (TREE_NUTS, 'Tree Nuts'),
+        (EGG, 'Eggs'),
+        (SHELLFISH, 'Shellfish'),
+        (FISH, 'Fish'),
+        (SESAME, 'Sesame'),
+    )
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    stars = models.IntegerField(choices=STAR_CHOICES, default=ZERO_STAR) # overall ease of eating there with given allergy
+    food_rating = models.IntegerField(default=0) # overall food rating
+    primary_allergen = models.CharField(max_length=10, choices=ALLERGEN_CHOICES, default=MILK)
+    description = models.CharField(max_length=500, default=None, blank=True, null=True)
+    review_date = models.DateTimeField("review date")
+    up_vote = models.IntegerField(default=0)
+    down_vote = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('-review_date',)
+
+class Review_Dish(models.Model):
+    """
+    A dish linked to a review
+    """
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    dish_name = models.CharField(max_length=50)
+    modifications = models.CharField(max_length=300, default=None, blank=True, null=True)
+    description = models.CharField(max_length=300, default=None, blank=True, null=True)
+    item_rating = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('dish_name',)
+
+class Saved_Dish(models.Model):
+    """
+    A model to track users' saved dishes
+    """
+    user = models.ForeignKey(User, models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL)
+    dish_name = models.CharField(max_length=50)
+    modifications = models.CharField(max_length=300, default=None, blank=True, null=True)
+    notes = models.CharField(max_length=300, default=None, blank=True, null=True)
+    date_added = models.DateTimeField('date added', default=timezone.now)
+
+    class Meta:
+        ordering = ('-date_added', 'dish_name')
+
+class Allergy(models.Model):
+    """
+    A model to track users' individual allergies
+    """
+    MILK = 'milk'
+    SOY = 'soy'
+    PEANUT = 'peanut'
+    WHEAT = 'wheat'
+    TREE_NUTS = 'tree nuts'
+    EGG = 'egg'
+    SHELLFISH = 'shellfish'
+    FISH = 'fish'
+    SESAME = 'sesame'
+    ALLERGEN_CHOICES = (
+        (MILK, 'Milk'),
+        (SOY, 'Soy'),
+        (PEANUT, 'Peanuts'),
+        (WHEAT, 'Wheat/Gluten'),
+        (TREE_NUTS, 'Tree Nuts'),
+        (EGG, 'Eggs'),
+        (SHELLFISH, 'Shellfish'),
+        (FISH, 'Fish'),
+        (SESAME, 'Sesame'),
+    )
+    allergy = models.CharField(max_length=10, choices=ALLERGEN_CHOICES, default=MILK)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    severity = models.IntegerField(default=0)
+    notes = models.CharField(max_length=300, default=None, blank=True, null=True)
+
+    class Meta:
+        ordering = ('-severity', 'allergy')
