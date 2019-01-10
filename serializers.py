55754f9a2f702dc40da6139c15c9465aafe905e1
Drew Stimson <drewstimson@Drews-MacBook-Air.local>
2019-01-09 22:06:40 -0500

    update requirements, add first draft of serializers

---
+++ serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from ratings.models import Restaurant, Review, Review_Dish, Saved_Dish, Allergy
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email", "is_staff")
+
+
+class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ("id", "name", "date_added", "street_address", "street_address_2",
+                  "city", "state", "country", "phone_number", "website", "google_maps_link")
+
+
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Review
+        fields = ("reviewer", "restaurant", "stars", "food_rating", "primary_allergen",
+                  "description", "review_date", "up_vote", "down_vote")
+
+
+class Review_DishSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Review_Dish
+        fields = ("dish_name", "modifications", "description", "item_rating")
+
+
+class Saved_DishSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Saved_Dish
+        fields = ("restaurant", "dish_name", "modifications", "notes", "date_added")
+
+
+class AllergySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Allergy
+        fields = ("allergy", "severity", "notes")
